492d3e249b64fc8c668dad91987004245c2282dd
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-23 14:29:26 +0330

    a032 move SECRET_KEY from settings.py to settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 
 
-
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-1r%tnk@im4n@uk5zx!q*i@wkr69darorwnglm%sa!_1ou=8#_w'
+
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
