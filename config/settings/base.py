7b5859bfdb74dcab0d645a88df52c013592d2443
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-23 16:24:46 +0330

    a042 move LANGUAGE_CODE = 'en-us' into config/settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
 
 
 AUTH_USER_MODEL = "accounts.CustomUser"
