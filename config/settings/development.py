5b81975d7d9acb75173d8f4225e9388c351b8a69
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-24 03:06:58 +0330

    a046 update config/settings/development.py DATABASES = { ... }

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,3 +2,17 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'acron',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'PORT': '3306',
+    }
+}
+
+
