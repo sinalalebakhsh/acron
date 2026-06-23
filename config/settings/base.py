d460473fae42f3c2811a3bc370ef4007f767408f
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-23 14:40:42 +0330

    a034 move ALLOWED_HOSTS = [] from settings.py to config/settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -7,6 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1r%tnk@im4n@uk5zx!q*i@wkr69darorwnglm%sa!_1ou=8#_w'
 
+ALLOWED_HOSTS = []
+
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
