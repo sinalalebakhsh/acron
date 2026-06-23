177c1d4cd7eeb83333ee481d00cf822ae8e5b344
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-23 12:00:15 +0330

    a029 add in config/settings/base.py
    DATABASES = {
        'default': {
            'ENGINE': 'django.db.backends.mysql',
            'NAME': 'acron',
            'HOST': 'localhost',
            'USER': 'root',
            'PASSWORD': '1234',
            'PORT': '3306',
        }
    }
    this is just for development mode
    this is not uses in production

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,5 +6,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'acron',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'PORT': '3306',
+    }
+}
 
 
