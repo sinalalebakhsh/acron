de3fc495453da781cec9b337b57e6b85bb79cfcc
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-24 04:17:00 +0330

    a068 apps/api/urls.py MIDIFICATION

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,11 +1,19 @@
-from django.http import HttpResponse
 from django.urls import path
-
-
-def api_home(request):
-    return HttpResponse("API Root")
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('', api_home),
+    path(
+        'token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+
+    path(
+        'token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
 ]
