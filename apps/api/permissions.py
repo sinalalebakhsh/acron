b0edc9c14b06857a220b10b57c60dc4bd7170768
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-25 01:28:45 +0330

    a078 in apps/api/permissions.py
    add class IsOwner(permissions.BasePermission):
    ...

--- apps/api/permissions.py
+++ apps/api/permissions.py
@@ -1,2 +1,10 @@
+from rest_framework import permissions
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+
 
 
