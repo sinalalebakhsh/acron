546e472fa7f14a3f704459ab55754b7cfd6bc943
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-24 18:00:06 +0330

    a070 apps/api/views.py MIDIFICATION

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+
+# 🔐 API محافظت‌شده
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def me(request):
+    return Response({
+        "id": request.user.id,
+        "username": request.user.username,
+        "email": request.user.email,
+    })
+
+
+
+
