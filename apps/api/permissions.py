73be5d69897603725608ec2f006b7933732b75c9
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-25 01:29:37 +0330

    a078 in apps/api/permissions.py
    add class IsAdminOrReadOnly(permissions.BasePermission):
    ...

--- apps/api/permissions.py
+++ apps/api/permissions.py
@@ -6,5 +6,11 @@ class IsOwner(permissions.BasePermission):
         return obj.user == request.user
 
 
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool((request.user and request.user.is_staff))
+
 
 
