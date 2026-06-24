2d6e91acd34f7333500f6595103ee15529062a87
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-24 17:59:32 +0330

    a069 apps/api/urls.py MIDIFICATION

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,19 +1,20 @@
 from django.urls import path
+from django.http import HttpResponse
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
-urlpatterns = [
-    path(
-        'token/',
-        TokenObtainPairView.as_view(),
-        name='token_obtain_pair'
-    ),
 
-    path(
-        'token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
+def api_root(request):
+    return HttpResponse("API is working 🚀")
+
+
+urlpatterns = [
+    path('', api_root),
+
+    # 🔐 JWT endpoints
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
