6b7c470d8eb21a5506db9c2a5e7b2baeef1ddfb0
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-24 21:14:07 +0330

    a074 add DEFAULT_PAGINATION_CLASS in acron/config/settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,7 +111,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination',
+    ),
+    'PAGE_SIZE': 10,
 }
 
 from datetime import timedelta
