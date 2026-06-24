a48ad2e1b9681fdc23baff2da92bd40214925347
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-24 04:16:34 +0330

    a067 apps/api/urls.py TEMPORARY MIDIFICATION

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,11 @@
+from django.http import HttpResponse
 from django.urls import path
 
-urlpatterns = []
+
+def api_home(request):
+    return HttpResponse("API Root")
 
 
-
+urlpatterns = [
+    path('', api_home),
+]
