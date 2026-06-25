7c54ab43c9ad4b5fe0f9063244820a414d68c757
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-25 11:38:03 +0330

    a082 UPDATE functionality from url: acron.com/api/me -> me function -> send to UserSerializer serializers.ModelSerializer

--- apps/api/views.py
+++ apps/api/views.py
@@ -2,17 +2,20 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-
+from . import serializers
 
 
 # 🔐 API محافظت‌شده
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def me(request):
+    serializer = serializers.UserSerializer(request.user)
+    return Response(serializer.data)
     return Response({
         "id": request.user.id,
         "username": request.user.username,
         "email": request.user.email,
+        
     })
 
 
