786b0671fc464d94dbbac29c47ac6fa43e885264
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-27 03:12:35 +0330

    a085 apps/customers/models.py MIDIFICATION this is important for future

--- apps/customers/models.py
+++ apps/customers/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+import uuid
+
+from config import settings
+
+
+
 # Create your models here.
+class Customer(models.Model):
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='customer'
+    )
+
+    phone_number = models.CharField(
+        max_length=255,
+        blank=True
+    )
+
+    birth_date = models.DateField(
+        null=True,
+        blank=True
+    )
+
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        editable=False
+    )
+
+    def __str__(self):
+        return self.user.username
