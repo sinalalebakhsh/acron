2314e7695a5dfb735d1e0ca0b94362ae9655039f
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-28 01:18:13 +0330

    a091 Add CustomerSerializer from serialiserz.ModelSerializer

---
+++ apps/customers/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from .models import Customer
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Customer
+
+        fields = [
+            'id',
+            'uuid',
+            'phone_number',
+            'birth_date',
+        ]
+
+
+
