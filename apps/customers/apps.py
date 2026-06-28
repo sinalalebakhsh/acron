34aa242e3e6e26d24f5a14a205c1df689ac425e2
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-29 03:20:16 +0330

    a0104 add 
        def ready(self):
            from . import signals
    
    to learning and register signals

--- apps/customers/apps.py
+++ apps/customers/apps.py
@@ -2,4 +2,13 @@ from django.apps import AppConfig
 
 
 class CustomersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.customers'
+
+    def ready(self):
+        from . import signals
+
+
+
+
+
