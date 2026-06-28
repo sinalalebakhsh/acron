0f3bfce9c389f92bd261b03bd09b81fd5cb9ba85
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-28 13:57:26 +0330

    a0101 Add first code in path: acron/apps/customers/signals.py

---
+++ apps/customers/signals.py
@@ -0,0 +1,24 @@
+# apps/customers/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from apps.accounts.models import CustomUser
+from .models import Customer
+
+@receiver(
+    post_save,
+    sender=CustomUser
+)
+def create_customer(
+    sender,
+    instance,
+    created,
+    **kwargs
+):
+    if created:
+
+        Customer.objects.create(
+            user=instance
+        )
+
+
