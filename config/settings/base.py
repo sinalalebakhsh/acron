5728edf43acacac8e5455ea7d722d9d891cb660a
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-30 14:19:56 +0330

    a0111 convert to comment this is path acron/config/settings/base.py DATABASES = {}

--- config/settings/base.py
+++ config/settings/base.py
@@ -67,9 +67,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DATABASES = {
-    'default': {}
-}
+# DATABASES = {}
 
 
 # Password validation
