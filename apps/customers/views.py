d965332931a62ba5ac83b5859c4ff3fbae294091
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-06-30 14:09:57 +0330

    a0110 best view for class CustomerMeView(APIView):

--- apps/customers/views.py
+++ apps/customers/views.py
@@ -10,9 +10,13 @@ class CustomerMeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-
         serializer = CustomerSerializer(request.user.customer)
+        return Response(serializer.data)
 
+    def patch(self, request):
+        serializer = CustomerSerializer(request.user.customer,data=request.data,partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         return Response(serializer.data)
 
 
