57cced6772ce328bf6061803b12e5ec2d3289c8f
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-02 16:45:58 +0330

    a0171 Create Product Media Inline Inherited from admin.TabularInline

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -19,3 +19,30 @@ class BrandAdmin(admin.ModelAdmin):
 
 
 
+# ۱. ساخت کلاس اینلاین برای گالری فرعی
+class ProductMediaInline(admin.TabularInline):
+    model = ProductMedia
+    extra = 1          # تعداد ردیف‌های خالی که به صورت پیش‌فرض نمایش داده می‌شود
+    max_num = 10       # قفل کردن فرانت‌اند ادمین روی حداکثر ۱۰ فایل فرعی
+
+
+# ۲. ساخت کلاس مدیریت اصلی محصول
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # الف) ستون‌های نمایشی در جدول لیست محصولات
+    list_display = ['name', 'brand', 'category', 'price', 'inventory', 'created_at']
+    
+    # ب) باکس فیلتر در سمت راست پنل ادمین
+    list_filter = ['category', 'brand', 'created_at']
+    
+    # ج) باکس جستجوی پیشرفته
+    search_fields = ['name', 'description']
+    
+    # د) پر شدن خودکار اسلاگ محصول بر اساس نام آن
+    prepopulated_fields = {'slug': ('name',)}
+    
+    # هـ) تزریق گالری فرعی به انتهای صفحه محصول
+    inlines = [ProductMediaInline]
+
+
+
