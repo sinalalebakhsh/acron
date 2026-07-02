874256578aadc7dd6359d14f1e192267fb0f815f
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-02 16:40:48 +0330

    a0170 import Models to Products app admin panel

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Category, Brand
+from .models import Category, Brand, Product, ProductMedia # مدل‌های جدید اضافه شدند
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
