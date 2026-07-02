0b5a1e6cfd98b11ad5e03500e9a0d0f72c95cd26
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-02 15:09:10 +0330

    a0163 register Category & Brand in Panel Admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Brand
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # ۱. مشخص کردن ستون‌هایی که در لیست ادمین نمایش داده می‌شوند
+    list_display = ['name', 'slug', 'parent']
+
+    # ۲. جادوی پر شدن خودکار اسلاگ بر اساس نام
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+
