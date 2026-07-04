a31d4808c715291af6907f571cc649be2640bd3f
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-04 03:39:32 +0330

    a 0174 add ProductListView

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
+from .models import Product
 
-# Create your views here.
+from .import serializers  
+
+class ProductListView(ListAPIView):
+    """
+    API دریافت لیست تمام محصولات فروشگاه
+    آزاد برای تمام کاربران (بدون نیاز به لاگین)
+    """
+    permission_classes = [AllowAny] # همه می‌توانند محصولات را ببینند
+    serializer_class = ProductSerializer
+    
+    # QuerySet کاملاً بهینه‌سازی شده برای جلوگیری از مشکل N+1
+    queryset = Product.objects.select_related(
+        'category', 
+        'brand'
+    ).prefetch_related(
+        'media_gallery'
+    ).all()
+
+
+
