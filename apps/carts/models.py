64d018abb15de62745439f71b330f7ae5560f6f6
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-06 16:38:34 +0330

    a0193 Create models for Cart App Cart -> models.Model CartItem -> models.Model These inherit

--- apps/carts/models.py
+++ apps/carts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+from apps.products.models import Product
+
+
+class Cart(models.Model):
+    # الف) جایگزینی 
+    # ID
+    #  عددی با 
+    # UUID
+    #  به عنوان کلید اصلی امنیتی
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class CartItem(models.Model):
+    # ب) اتصال آیتم به سبد خرید
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    
+    # ج) اتصال آیتم به محصول
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    
+    # د) تعداد محصول در سبد
+    quantity = models.PositiveSmallIntegerField(default=1)
+
+    class Meta:
+        # هـ) جلوگیری از ساخت دو ردیف برای یک محصول تکراری در یک سبد
+        unique_together = [['cart', 'product']]
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"# Create your models here.
