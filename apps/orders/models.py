e697a3bd40fb64db3b36f3b1ed39ce3d831c6a3d
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-09 12:57:21 +0330

    a0206 Create models: order order item

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,54 @@
+import uuid
 from django.db import models
+from apps.customers.models import Customer
+from apps.products.models import Product
 
-# Create your models here.
+
+
+class Order(models.Model):
+    # ۱. تعریف وضعیت‌های مختلف یک سفارش با استفاده از TextChoices
+    class OrderStatus(models.TextChoices):
+        PENDING = 'P', 'در انتظار پرداخت'
+        COMPLETED = 'C', 'پرداخت موفق'
+        CANCELED = 'X', 'لغو شده'
+
+    # ۲. شناسه یکتا و غیرقابل حدس برای پیگیری سفارش
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
+    # ۳. ارتباط با مشتری (سفارش برخلاف سبد خرید، حتماً صاحب دارد)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, related_name='orders')
+    
+    # ۴. وضعیت فعلی سفارش
+    status = models.CharField(
+        max_length=1, 
+        choices=OrderStatus.choices, 
+        default=OrderStatus.PENDING
+    )
+    
+    # ۵. زمان ثبت سفارش
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} - {self.customer.user.username}"
+
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='order_items')
+    quantity = models.PositiveSmallIntegerField()
+    
+    # 6. The most important field of this phase: Freezing the price!
+    # ۶. مهم‌ترین فیلد این فاز: فریز کردن قیمت!
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.product.name} (x{self.quantity})"
+
+
+
+
+
+
+
+
