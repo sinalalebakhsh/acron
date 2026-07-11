38894b5559af66a7cb630d21915d9db94c4da581
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-11 17:19:14 +0330

    a0237 add documentation for development.py & production.py

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,9 +1,27 @@
+# development.py is a settings file for the development environment in a Django project. 
+# It contains configuration settings that are specific to the development environment, 
+# such as enabling debug mode, configuring the database connection, 
+# and other settings that are suitable for local development.
+# this is the settings file for the development environment, 
+# which is used when running the Django project locally on a developer's machine.
+
+# why import * from base.py?
+# The import statement from .base import * is used to import all the settings defined in the
 from .base import *
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+# we can use SQLite for development, which is a lightweight database that doesn't require a separate server.
+# or we can use MySQL for development, which is a more robust database that requires a separate server.
+# or we can use PostgreSQL for development, which is a powerful database that requires a separate server.
+# PostgreSQL is a good choice for development,
+# because it is a powerful database that requires a separate server.
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
--- config/settings/production.py
+++ config/settings/production.py
@@ -1,2 +1,11 @@
+# Production settings
+
+# Debug mode should be turned off in production for security reasons.
 DEBUG = False
+
+# ALLOWED_HOSTS is a list of strings representing the host/domain names that this Django site can serve.
+# In production, you should set this to the actual domain names of your site.
 ALLOWED_HOSTS = ['acronproject.com', 'www.acronproject.com', 'acronproject.com', 'www.acronproject.com', 'localhost', '127.0.0.1']
+
+
+
