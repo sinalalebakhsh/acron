98f1a85b5e1d34c04375c28fc97474440c45d94a
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-13 22:26:01 +0330

    a0271 add Payment Admin in admin panel

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['transaction_id', 'order', 'amount', 'status', 'created_at']
+    list_filter = ['status', 'created_at']
+    search_fields = ['transaction_id', 'order__id']
+    
+    # تمام فیلدهای مالی را برای ادمین Read-Only می‌کنیم تا امنیت حفظ شود
+    readonly_fields = ['transaction_id', 'order', 'amount', 'status', 'gateway_url', 'created_at', 'updated_at']
+
+    def has_add_permission(self, request):
+        return False # ادمین نباید بتواند دستی تراکنش مالی خلق کند
+
+    def has_delete_permission(self, request, obj=None):
+        return False # تراکنش مالی هرگز نباید حذف شود
+
+
