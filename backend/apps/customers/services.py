fa5c754cfcbf4153a6ea7e9bf31b0db4ed0561f3
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-07-27 22:39:54 +0330

    a0358-backend:Create services.py in customers APP

---
+++ backend/apps/customers/services.py
@@ -0,0 +1,22 @@
+from django.db import transaction
+from .models import Customer, Address
+
+class AddressService:
+    @staticmethod
+    @transaction.atomic
+    def set_default_address(user, address_id):
+        """
+        تنظیم آدرس پیش‌فرض برای کاربر و غیرفعال کردن بقیه آدرس‌ها
+        """
+        customer = Customer.objects.get(user=user)
+        
+        # تمام آدرس‌های فعلی کاربر از حالت پیش‌فرض خارج می‌شوند
+        Address.objects.filter(customer=customer, is_default=True).update(is_default=False)
+        
+        # آدرس انتخابی پیش‌فرض می‌شود
+        address = Address.objects.get(id=address_id, customer=customer)
+        address.is_default = True
+        address.save()
+        return address
+
+
