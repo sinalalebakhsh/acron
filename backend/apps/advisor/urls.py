9a1878979775c52cd90339709a5d30cd4ab05ee2
Sina Lalebakhsh <lalebakhshsina@gmail.com>
2026-08-18 02:46:15 +0330

    a0402-backend: urls.py modified

--- backend/apps/advisor/urls.py
+++ backend/apps/advisor/urls.py
@@ -1,16 +1,21 @@
-# apps/advisor/urls.py
-
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import AdvisorViewSet
 
-# استفاده از DefaultRouter برای ساخت خودکار مسیرهای استاندارد RESTful
+
 router = DefaultRouter()
-router.register(r'advisor', AdvisorViewSet, basename='advisor')
+
+router.register(
+    r"",
+    AdvisorViewSet,
+    basename="advisor",
+)
+
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path(
+        "",
+        include(router.urls),
+    ),
 ]
-
-
-
